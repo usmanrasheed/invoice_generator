f6cfcb52326a44e66d75fde359f9e5baf2bfc74e
usmanrasheed <usmanrasheed400@gmail.com>
2025-06-05 19:07:49 +0500

    theme update

--- lib/res/colors/app_color.dart
+++ lib/res/colors/app_color.dart
@@ -3,17 +3,18 @@ import 'package:flutter/material.dart';
 class AppColor{
   static const Color blackColor = Color(0xff000000);
   static const Color whiteColor = Color(0xffffffff);
-  static const Color errorColor = Color(0xffF08080);
+  static const Color errorColor = Color(0xFFF08080);
+  //34C759 //green
+  static const Color primaryDarkColor = Color(0xff173787);//2a3655
+  static const Color primaryColor = Color(0xff237ee2);//237ee2
+  static const Color accentColor = Color(0xffE5F1FB);
+  static const Color backgroundColor = Color(0xFFF9FAFB);
 
-  static const Color primaryColor = Color(0xff35B0AB);
-  static const Color primaryDarkColor = Color(0xff1e6e6a);
-  static const Color accentColor = Color(0xff174A4A);
-  static const Color backgroundColor = Color(0xFFfaffff);
-  static const Color disableColor = Color(0xff35B0AB);
+  static const Color disableColor = Color(0xffE5F1FB);
   static const Color optionalFieldFillColor = Color(0xffe1f0ef);
 
-  //static const Color accentColor = Color(0xffA3C6E0);
-  //static const Color secondaryColor = Color(0xff4FC3F7);
-  //static const Color mainColor = Color(0xff003A70);
+//static const Color accentColor = Color(0xffA3C6E0);
+//static const Color secondaryColor = Color(0xff4FC3F7);
+//static const Color mainColor = Color(0xff003A70);
 
 }
